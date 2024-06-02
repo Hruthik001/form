ea1843afd9090236338a25f9149d101f5cfaa7ac
Hruthik001 <168667727+Hruthik001@users.noreply.github.com>
2024-06-02 04:47:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function takevalue(){
+    let firstName = document.getElementById
+    ("firstName").value;
+
+    let lastName = document.getElementById
+    ("lastName").value;
+
+    let gender = document.getElementById
+    ("gender").value;
+
+    let studentID = document.getElementById
+    ("studentID").value;
+
+    let Email = document.getElementById
+    ("email").value;
+
+    let Classes = document.getElementById
+    ("classes").value;
+    
+    document.write("First name =" + firstName + "<br>");
+
+    document.write("Last name ="+ lastName + "<br>");
+
+    document.write("Gender ="+ lastName + "<br>");
+
+
+    document.write("StudentID ="+ studentID + "<br>");
+
+    document.write("Email="+ Email + "<br>");
+
+    document.write("Classes ="+ Classes + "<br>");
+}
